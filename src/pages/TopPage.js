69e503b0392bf7de126ae8d2f2a2de7fd75c5963
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-06-19 21:00:31 +0900

    a

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import {fetchGetData} from '../apis/index'
+import { Store } from '../store/index'
+import { GET_DATA } from '../actions/index'
+import Card from '../components/Card'
+
 const TopPage = () => {
+    const {globalState, setGlobalState} = useContext(Store)
+    useEffect(() => {
+        fetchGetData().then(res => {
+            setGlobalState({
+            type: GET_DATA,
+            data: res.data
+        })
+    })
+},[])
+console.log(globalState)
+return(
+<div style={{display: 'flex',flexWrap:'wrap'}}>
+{
+    globalState.user_data.map((user, index) => {
     return (
-        <div>
-            <h1>TopPage</h1>
-        </div>
-    )
-}
+        <Card user={user} key={index} />
+        )
+    })
+    }
+</div>
+
+)}
 export default TopPage
