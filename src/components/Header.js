26b46805f4530677b73ab5ecc434edabf3f0f967
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-07-03 20:58:14 +0900

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {AppBar, Toolbar} from '@material-ui/core'
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <AppBar position="static">
         <Toolbar>
             <h3>GlobalState</h3>
+            <Link to={'/'}>
+            <a style={{color: "yellow"}}>Top</a>
+            </Link>
+
+            <Link to={'/Second'}> 
+            <a style={{color: "yellow"}}>Second</a>
+            </Link>
         </Toolbar>
         </AppBar>
     );
